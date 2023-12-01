e304f1f63b9fa2842807eb5c4c38b7147af3f122
arTiwnl <contatocarvalhoarthur@gmail.com>
2023-11-30 23:11:31 -0300

    feat:atualizacoes_e

--- classes-e-atributos/script.js
+++ classes-e-atributos/script.js
@@ -13,3 +13,31 @@ sessao.classList.remove('ativo'); //remover classe
 sessao.classList.toggle('ativo'); //adiciona/remove a classe
 sessao.classList.contains('ativo'); //true ou false
 sessao.classList.replace('ativo', 'inativo');
+
+
+
+//getAttribute e setAttribute
+
+/*Métodos que retornam ou definem de acordo com o atributo selecionado*/
+
+const img = document.querySelector('img');
+
+img.getAttribute('src'); //valor do src
+img.setAttribute('alt', 'Texto Alternativo'); //muda o alt
+img.hasAttribute('id'); //true / false
+img.removeAttribute('alt'); //remove o alt
+
+img.hasAttributes(); //true / false se tem algum atributo
+
+//é muito comum métodos de get e set
+
+//Read Only vs Writable
+
+/* Existem propriedades que não permitem a mudança de seus valores,
+essas são considerados Read Only ou apenas leitura.*/
+
+sessao.className; // string com o nome das classes
+sessao.className = 'azul'; //substitui completament a string
+sessao.className += 'vermelho'; //adiciona azul à string
+
+sessao.attributes = 'class="ativo"'; // Não funciona, read-only
